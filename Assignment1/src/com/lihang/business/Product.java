b9173f39aa9c69944840ce554b5a80043d3f214f
Lihang Zhou <zhou.liha@husky.neu.edu>
2019-09-19 20:29:06 -0400

    first commit

---
+++ Assignment1/src/com/lihang/business/Product.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lihang.business;
+
+/**
+ *
+ * @author lihang
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availNum;
+    private String color;
+    private String mdl;
+    private String sn;
+    private String len;
+    private String width;
+    private String hgt;
+    private String origin;
+    private String proDate;
+    private String salDate;
+    private String rudder;
+    private String ifUsed;
+    private String picURL;
+    private String description;
+
+    public String getPicURL() {
+        return picURL;
+    }
+
+    public void setPicURL(String picURL) {
+        this.picURL = picURL;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMdl() {
+        return mdl;
+    }
+
+    public void setMdl(String mdl) {
+        this.mdl = mdl;
+    }
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+
+    public String getLen() {
+        return len;
+    }
+
+    public void setLen(String len) {
+        this.len = len;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public String getHgt() {
+        return hgt;
+    }
+
+    public void setHgt(String hgt) {
+        this.hgt = hgt;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getProDate() {
+        return proDate;
+    }
+
+    public void setProDate(String proDate) {
+        this.proDate = proDate;
+    }
+
+    public String getSalDate() {
+        return salDate;
+    }
+
+    public void setSalDate(String salDate) {
+        this.salDate = salDate;
+    }
+
+    public String getRudder() {
+        return rudder;
+    }
+
+    public void setRudder(String rudder) {
+        this.rudder = rudder;
+    }
+
+    public String getIfUsed() {
+        return ifUsed;
+    }
+
+    public void setIfUsed(String ifUsed) {
+        this.ifUsed = ifUsed;
+    }
+    
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
