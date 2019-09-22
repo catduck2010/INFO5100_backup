891cd2ee8fd1fda74a7e004d9cfd6e205faef83e
Lihang Zhou <zhou.liha@husky.neu.edu>
2019-09-22 16:06:26 -0400

    lab 2.5

---
+++ Lab2.5/src/com/lihang/business/VitalSignHistory.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lihang.business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lihang
+ */
+public class VitalSignHistory {
+    private ArrayList<VitalSign> vitalSignHistory;
+    
+    
+    public VitalSignHistory(){
+        this.vitalSignHistory=new ArrayList<>();
+    }
+
+    public ArrayList<VitalSign> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+
+    public void setVitalSignHistory(ArrayList<VitalSign> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+    
+    public VitalSign addVitalSign(){
+        VitalSign vs=new VitalSign();
+        this.vitalSignHistory.add(vs);
+        return vs;
+    }
+    
+    public void deleteVitalSign(VitalSign vs){
+        this.vitalSignHistory.remove(vs);
+    }
+    
+}
